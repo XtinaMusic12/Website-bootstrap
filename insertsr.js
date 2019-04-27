c993ce691de9326d3b71e2ae96039473d41db719
Christina Roufosse <roufosse.christina@gmail.com>
2019-04-27 12:51:51 -0700

    Updating changes

--- insertsr.js
+++ insertsr.js
@@ -1,39 +1,23 @@
 var srbutton = document.getElementById('insertsr');
-var item1, item2, item3, item4, item5;
-var text1, text2, text3, text4, text5;
+var spn;
 
 srbutton.addEventListener("click", Insert_SR, false);
 
 
 function Insert_SR() {
-    item1 = prompt("Add a special request to the Menu: you can add up to 5.");
-        text1 = "<ul>";
-        text1 += "<li>" + item1 + "</li>";
-        text1 = "</ul>";
-    document.getElementById("item1").innerHTML = text1;
-}
+    // Prompt User for Special Request:
+    spn = prompt("Add a special request to the Menu: you can add up to 5.");
+    // create new li elements:
+    var spnli = document.createElement("li");
+    // add its content:
+    var newSR = document.createTextNode(spn);
+    // add the text to the newly created li:
+    spnli.appendChild(newSR);
 
+    // add the newly created element and its content into the DOM 
+    var currentLi = document.getElementById("specialrequest");
+    document.body.insert(spnli, currentLi);
 
-function SR_P2() {
-    item2 = prompt("Add a special request to the Menu: you can add up to 5.");
-    document.getElementById("item2").innerHTML = item2;
-}
-
-
-function SR_P3() {
-    item3 = prompt("Add a special request to the Menu: you can add up to 5.");
-    document.getElementById("item3").innerHTML = item3;
-}
-
-
-function SR_P4() {
-    item4 = prompt("Add a special request to the Menu: you can add up to 5.");
-    document.getElementById("item4").innerHTML = item4;
-}
-
-
-function SR_P5() {
-    item5 = prompt("Add a special request to the Menu: you can add up to 5.");
-    document.getElementById("item5").innerHTML = item5;
-}
+    // spnli.document.getElementById("specialrequest").appendChild(spn);
+} 
 
