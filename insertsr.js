f9d75592b52f9154ea98adb0f6afbbb7596eeade
Christina Roufosse <roufosse.christina@gmail.com>
2019-04-27 19:31:04 -0700

    Extra Assignment-ConFusion 5 max delete all

--- insertsr.js
+++ insertsr.js
@@ -5,17 +5,24 @@ var newSR;
 var ordernb = 0;
 var newcount;
 var rsrbutton = document.getElementById('removesr');
+var delbutton = document.getElementById('deleteall');
+var list = document.getElementById('specialrequest');
 
 srbutton.addEventListener("click", Insert_SR, false);
 rsrbutton.addEventListener("click", Remove_LastItem, false);
+delbutton.addEventListener("click", deleteAll, false);
 
 function Insert_SR() {
-    // Prompt User for Special Request:
-    spn = prompt("Add a special request to the Menu: you can add up to 5.");
     // incrementing order number:
     ordernb++;
     // add the new count in the DOM:
     document.getElementById("ordernb").innerHTML = "Number of Special Requests: " + ordernb;
+
+
+    // Prompt User for Special Request:
+    spn = prompt("Add a special request: you can add up to 5 orders.");
+
+
     // create new li elements:
     newLi = document.createElement("li");
     // add its content:
@@ -24,8 +31,13 @@ function Insert_SR() {
     newLi.appendChild(newSR);
 
     // add the newly created element and its content into the DOM 
-
     document.getElementById("specialrequest").appendChild(newLi);
+
+    // Not letting customer order more than 5 Special Requests:
+    if (ordernb === 5) {
+        alert('You have reached the maximum special request orders. You can remove your last order and replace it.' + '\n\n' + 'Click on "Start Over" to write a new special requests list.');
+        // srbutton.removeEventListener("click", Insert_SR);
+    }
 }
 
 function Remove_LastItem() {
@@ -37,3 +49,9 @@ function Remove_LastItem() {
     document.getElementById("ordernb").innerHTML = "Number of Special Requests: " + ordernb;
 } 
 
+function deleteAll() {
+    list.parentNode.removeChild(list);
+    ordernb = 0;
+    // add the new count in the DOM:
+    document.getElementById("ordernb").innerHTML = "Number of Special Requests: " + ordernb;
+}
