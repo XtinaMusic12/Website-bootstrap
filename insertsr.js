48b6cdc9e8d3cd12b32b7bc3a7efe04aaf40c2fa
Christina Roufosse <roufosse.christina@gmail.com>
2019-04-29 12:51:39 -0700

    Extra Assignment ConFusion Remove Event Listener at 5

--- insertsr.js
+++ insertsr.js
@@ -13,31 +13,33 @@ rsrbutton.addEventListener("click", Remove_LastItem, false);
 delbutton.addEventListener("click", deleteAll, false);
 
 function Insert_SR() {
-    // incrementing order number:
-    ordernb++;
-    // add the new count in the DOM:
-    document.getElementById("ordernb").innerHTML = "Number of Special Requests: " + ordernb;
-
-
-    // Prompt User for Special Request:
-    spn = prompt("Add a special request: you can add up to 5 orders.");
-
-
-    // create new li elements:
-    newLi = document.createElement("li");
-    // add its content:
-    newSR = document.createTextNode(spn);
-    // add the text to the newly created li:
-    newLi.appendChild(newSR);
-
-    // add the newly created element and its content into the DOM 
-    document.getElementById("specialrequest").appendChild(newLi);
 
     // Not letting customer order more than 5 Special Requests:
     if (ordernb === 5) {
         alert('You have reached the maximum special request orders. You can remove your last order and replace it.' + '\n\n' + 'Click on "Start Over" to write a new special requests list.');
-        // srbutton.removeEventListener("click", Insert_SR);
+        srbutton.removeEventListener("click", Insert_SR);
     }
+
+    // Prompt User for Special Request:
+    else {
+        // incrementing order number:
+        ordernb++;
+        // add the new count in the DOM:
+        document.getElementById("ordernb").innerHTML = "Number of Special Requests: " + ordernb;
+        spn = prompt("Add a special request: you can add up to 5 orders.");
+
+
+        // create new li elements:
+        newLi = document.createElement("li");
+        // add its content:
+        newSR = document.createTextNode(spn);
+        // add the text to the newly created li:
+        newLi.appendChild(newSR);
+
+        // add the newly created element and its content into the DOM 
+        document.getElementById("specialrequest").appendChild(newLi);
+    }
+
 }
 
 function Remove_LastItem() {
@@ -47,7 +49,8 @@ function Remove_LastItem() {
     ordernb -= 1;
     // add the new count in the DOM:
     document.getElementById("ordernb").innerHTML = "Number of Special Requests: " + ordernb;
-} 
+    srbutton.addEventListener("click", Insert_SR, false);
+}
 
 function deleteAll() {
     list.parentNode.removeChild(list);
