ea31dc618d53ba9b3aea14272b060a685cffbb9c
Christina Roufosse <roufosse.christina@gmail.com>
2019-05-03 23:04:11 -0700

    Extra Assignment 2 Menu Fixed buttons

--- script2.js
+++ script2.js
@@ -4,7 +4,11 @@ var orderburger = 0;
 // for the total calculation
 var pizza = 12;
 var burger = 5;
+
 var ordertotal = pizza * orderpizza + burger * orderburger;
+
+// var integerp = parseInt(orderpizza, 10);
+// var integerb = parseInt(orderburger, 10);
 
 // adding event listeners on button click:
 // var btnremovepizza = document.getElementByID('removepizza');
@@ -12,43 +16,58 @@ var ordertotal = pizza * orderpizza + burger * orderburger;
 // var btnremoveburger = document.getElementByID('removeburger');
 // var btnaddburger = document.getElementByID('addburger');
 
-// btnremovepizza.addEventListener("click", Remove_Pizza, false);
-// btnaddpizza.addEventListener("click", Add_Pizza, false);
-// btnremoveburger.addEventListener("click", Remove_Burger, false);
-// btnaddburger.addEventListener("click", Add_Burger, false);
+document.getElementByID('removepizza').addEventListener("click", Remove_Pizza);
+document.getElementByID('addpizza').addEventListener("click", Add_Pizza);
+document.getElementByID('removeburger').addEventListener("click", Remove_Burger);
+document.getElementByID('addburger').addEventListener("click", Add_Burger);
 
 // functions:
 function Remove_Pizza() {
 
-    if (orderpizza < 0) {
-        orderpizza = 0;
+    if (orderpizza == 0) {
+
+        document.getElementById("orderpizza").value = 0;
+
         document.getElementById("total").innerHTML = "Total Price " + ordertotal + " dollars.";
+
     } else {
-        document.getElementById("orderpizza").value = --orderpizza;
+        orderpizza--;
+
+        document.getElementById("orderpizza").value = orderpizza;
 
         document.getElementById("total").innerHTML = "Total Price " + ordertotal + " dollars.";
     }
 }
 
 function Add_Pizza() {
-    document.getElementById("orderpizza").value = orderpizza++;
+    orderpizza++;
+
+    document.getElementById("orderpizza").value = orderpizza;
 
     document.getElementById("total").innerHTML = "Total Price " + ordertotal + " dollars.";
 }
 
 function Remove_Burger() {
 
-    if (orderburger < 0) {
-        orderburger = 0;
+    if (orderburger == 0) {
+
+        document.getElementById("orderburger").value = 0;
+
         document.getElementById("total").innerHTML = "Total Price " + ordertotal + " dollars.";
+
     } else {
-        document.getElementById("orderbuger").innerHTML = --orderburger;
+        orderburger--;
+
+        document.getElementById("orderbuger").value = orderburger;
+
         document.getElementById("total").innerHTML = "Total Price " + ordertotal + " dollars.";
     }
 }
 
 function Add_Burger() {
-    document.getElementById("orderburger").innerHTML = ++orderburger;
+    orderburger++;
+
+    document.getElementById("orderburger").value = orderburger;
 
     document.getElementById("total").innerHTML = "Total Price " + ordertotal + " dollars.";
 }
